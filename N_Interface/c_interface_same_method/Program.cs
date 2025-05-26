8121bdd3d1db2ef5531ffa1da6e25485711dca37
Vijaya Malla <mallabiju@gmail.com>
2025-05-26 20:05:46 +0545

    Interface

---
+++ N_Interface/c_interface_same_method/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace c_interface_same_method;
+
+interface IStudent
+{
+    public void exam();
+}
+
+interface ITeacher
+{
+    public void exam();
+}
+
+class College : IStudent, ITeacher
+{
+    void IStudent.exam()
+    {
+        Console.WriteLine("I'm a Student.");
+    }
+    void ITeacher.exam()
+    {
+        Console.WriteLine("I'm a Teacher.");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        IStudent s1 = new College();
+        ITeacher t1 = new College();
+        s1.exam();
+        t1.exam();
+    }
+}
---
+++ N_Interface/d_interface_inherit_interface/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace d_interface_inherit_interface;
+
+interface IOne
+{
+    public void MethodOne();
+}
+interface ITwo
+{
+    public void MethodTwo();
+}
+interface IThree : IOne, ITwo
+{
+    public void MethodThree();
+}
+class Program : IThree
+{
+    void IOne.MethodOne()
+    {
+        Console.WriteLine("I'm body from Interface IOne");
+    }
+    void ITwo.MethodTwo()
+    {
+        Console.WriteLine("I'm body from Interface ITwo");
+    }
+    void IThree.MethodThree()
+    {
+        Console.WriteLine("I'm body from Interface IThree");
+    }
+    static void Main(string[] args)
+    {
+        IThree i = new Program();
+        i.MethodOne();
+        i.MethodTwo();
+        i.MethodThree();
+    }
+}
