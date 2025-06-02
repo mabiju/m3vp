6914ac9ce8ea2d823984d5f904db27c14b97f84a
Vijaya Malla <mallabiju@gmail.com>
2025-06-02 22:12:47 +0545

    New updates

---
+++ P_Abstraction/b_abstraction/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace b_abstraction;
+
+abstract class MyClass
+{
+    // public virtual int calc(int a, int b)
+    public int calc(int a, int b)
+    {
+        return (a + b);
+    }
+}
+
+class YourClass : MyClass
+{
+    // public override int calc(int a, int b)
+    public int mul(int a, int b)
+    {
+        return (a * b);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        YourClass obj = new YourClass();
+        int result = obj.calc(12, 34);
+        Console.WriteLine("The result = {0}", result);
+    }
+}
---
+++ P_Abstraction/c_abstraction/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace c_abstraction;
+
+abstract class MyClass
+{
+    public abstract void calculate(double a);
+}
+
+class SubclassOne : MyClass
+{
+    public override void calculate(double a)
+    {
+        Console.WriteLine("Square root = " + Math.Sqrt(a));
+    }
+}
+class SubclassTwo : MyClass
+{
+    public override void calculate(double a)
+    {
+        Console.WriteLine("Square = " + (a * a));
+    }
+}
+class SubclassThree : MyClass
+{
+    public override void calculate(double a)
+    {
+        Console.WriteLine("Cube = " + (a * a * a));
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        SubclassOne obj1 = new SubclassOne();
+        SubclassTwo obj2 = new SubclassTwo();
+        SubclassThree obj3 = new SubclassThree();
+
+        obj1.calculate(9);
+        obj2.calculate(9);
+        obj3.calculate(9);
+    }
+}
